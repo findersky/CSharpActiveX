59a1fbea0a94bf6c2c6055d9dd316f1410eabe25
findersky <skyfinder@qhjsw.net>
2017-03-20 01:03:03 +0800

    New projects

---
+++ CSharpActiveX/IPageExecuteJavaScriptEvents.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace CSharpActiveX
+{
+    [Guid("a5ee0756-0cbb-4cf1-9a9c-509407d5eed6")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    interface IPageExecuteJavaScriptEvents
+    {
+        
+        [DispId(1)]
+        object CompletedCallBack { get; set; }
+    }
+}
